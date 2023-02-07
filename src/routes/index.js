627ac55b1b1a05801896380472edc492b6f43799
Carl Llemos <llemoscarl671@gmail.com>
2023-02-07 09:23:44 +0800

    updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,9 @@
 const app = require('express')
 const router = app.Router()
 
-// base url
-const base_url = "http://localhost:1000"
-
 router.get('/', (req, res) => {
     res.render('index', {
         title: 'Portfolio',
-        base_url: base_url
     })
 })
 
